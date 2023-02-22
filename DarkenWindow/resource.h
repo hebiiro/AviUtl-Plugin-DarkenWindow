bbca523fd071cdd157d6773aafe0d32b10f2c2b1
hebiiro <96464759+hebiiro@users.noreply.github.com>
2023-02-22 16:53:38 +0900

    7.2.0
    レイヤーの色分けができる機能を追加

--- DarkenWindow/resource.h
+++ DarkenWindow/resource.h
@@ -3,6 +3,7 @@
 // DarkenWindow.rc で使用
 //
 #define IDS_INFORMATION                 101
+#define IDC_CHOOSE_COLOR                172
 
 // Next default values for new objects
 // 
@@ -10,7 +11,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
